817f24811499f64ea7804674b5d4c128ec9b1dd9
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-09 22:29:49 -0500

    tweaking descriptions

--- src/core/item/clothing/WornLeathers.ts
+++ src/core/item/clothing/WornLeathers.ts
@@ -8,7 +8,8 @@ class WornLeathers extends ItemEquippable{
         super({
             id: ItemId.WornLeathers,
             title: 'Worn Leathers',
-            description: 'A set of hardened animal hide braces that cover the chest, arms and legs (+20% Physical Resistance)',
+            description: `A set of hardened animal hide braces that cover the chest, arms and legs 
++20% Physical Resistance`,
             slotType:'armor',
             onAddBonuses:function(stats:ICreatureStatSet){
                 stats.Resistances.Physical += 0.2;
--- src/core/item/weapons/SonicLongsword.ts
+++ src/core/item/weapons/SonicLongsword.ts
@@ -10,7 +10,7 @@ import { DamageFuncBag } from '../WeaponAttackStep';
 export default new Weapon({
     id: ItemId.SonicLongsword,
     title: 'Sonic Longsword',
-    description: 'A blade whose hilt generates an electric charge which is inflicted on enemies',
+    description: `A blade whose hilt generates an electric charge which is inflicted on enemies`,
     damageBlocked: 0.05,
     useRequirements:{
         Agility: 20
