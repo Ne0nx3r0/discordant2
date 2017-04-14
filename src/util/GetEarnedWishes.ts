f156da399a22b1eabce8d850089e2543ab282154
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-14 16:23:50 -0500

    more wish work

---
+++ src/util/GetEarnedWishes.ts
@@ -0,0 +1,30 @@
+import CreatureAIControlled from '../core/creature/CreatureAIControlled';
+
+import PartyExplore from '../bot/commands/PartyExplore';
+import { IBattlePlayerCharacter } from '../core/battle/PlayerBattle';
+import Creature from '../core/creature/Creature';
+import PlayerBattle from '../core/battle/PlayerBattle';
+import { XPToLevel } from "./XPToLevel";
+
+const PARTY_PENALTY = 0.015;
+
+interface WishesBag{
+    baseWishes:number;
+    partySize:number;
+    highestLevel:number;
+    playerLevel:number;
+}
+
+export default function GetEarnedWishes(bag:WishesBag){
+    // Calculate the party penalty
+    const partyPenalty = 1 - bag.partySize * PARTY_PENALTY;
+
+    // Adjust base wishes
+    const adjustedWishes = bag.baseWishes * partyPenalty;
+
+    // Calculate what percent to level the highest level player is getting
+    const wishesPercent = adjustedWishes / XPToLevel[bag.highestLevel];
+    
+    //Adjust base wishes to match the highest level player's percent
+    return XPToLevel[bag.playerLevel] * wishesPercent;
+}
