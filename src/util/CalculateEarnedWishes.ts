16f355c54e75823a076c848924a9bc4bc4c994a6
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-14 00:18:39 -0500

    lvl peanlty upate

--- src/util/CalculateEarnedWishes.ts
+++ src/util/CalculateEarnedWishes.ts
@@ -5,7 +5,7 @@ import { IBattlePlayerCharacter } from '../core/battle/PlayerBattle';
 import Creature from '../core/creature/Creature';
 
 const PARTY_PENALTY = 0.02;
-const LEVEL_PENALTY = 0.01;
+const LEVEL_PENALTY = 0.03;
 
 export default function CalculateEarnedWishes(bpcs:Map<Creature, IBattlePlayerCharacter>,opponent:CreatureAIControlled){
     const baseWishes = opponent.wishesDropped;
