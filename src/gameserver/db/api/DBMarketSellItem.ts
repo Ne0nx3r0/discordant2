5ee9e9823d23d7e7553e7e90dd869ddab364af15
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-17 22:06:00 -0500

    fx

--- src/gameserver/db/api/DBMarketSellItem.ts
+++ src/gameserver/db/api/DBMarketSellItem.ts
@@ -3,14 +3,14 @@ import { DBPlayer } from '../DBInterfaces';
 import { MarketSellData } from '../../socket/requests/MarketSellRequest';
 
 const queryStr = `
-    SELECT market_sell_item($1,$2,$3,$4) as offerId;
+    SELECT market_sell_item($1,$2,$3,$4) as offerid;
 `;
 
 export default async function DBMarketSellItem(db:DatabaseService,bag:MarketSellData):Promise<string>{
     try{
         const result = await db.getPool().query(queryStr,[bag.uid,bag.item,bag.amount,bag.price]);
 
-        return result.rows[0].offerId;
+        return result.rows[0].offerid;
     }
     catch(ex){
         //Kind of hackish - "custom" exception from transfer_player_item function
