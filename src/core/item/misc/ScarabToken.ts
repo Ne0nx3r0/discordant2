42b534a6dd2aa6c06924a44d4a5b0ec6cf8890dd
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-26 13:28:49 -0500

    desc

--- src/core/item/misc/ScarabToken.ts
+++ src/core/item/misc/ScarabToken.ts
@@ -5,5 +5,5 @@ import InventoryItem from "../InventoryItem";
 export default new ItemBase({
     id: ItemId.ScarabToken,
     title: 'Scarab Token',
-    description: 'Said to be the currency of an ancient civlization, these tokens still hold value among certain collectors who tend to be quite secretive about their purpose.',
+    description: 'A small golden scarab. Said to be the currency of an ancient civlization, these tokens still hold value among certain collectors who tend to be quite secretive about their purpose.',
 });
