25a92ef20555a5c79c41750234f589757cbaba58
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-29 18:38:37 -0500

    yerba

---
+++ src/core/map/tiles/EventTileForagable.ts
@@ -0,0 +1,24 @@
+import EventTile from '../EventTile';
+import ItemId from '../../item/ItemId';
+
+export function EventTileForagable(){
+    return new EventTile({
+        onEnter: function(bag){
+            if(bag.runCount == 0){
+                bag.sendPartyMessage(`There's a small patch of yerba shrubs.`);
+            }
+        },
+        onInteract: function(bag){
+            if(bag.runCount == 0){
+                bag.sendPartyMessage(`${bag.player.title} found a yerba flower!`);
+
+                bag.game.grantPlayerItem(bag.player.uid,ItemId.Yerba,1);
+
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    });
+}
