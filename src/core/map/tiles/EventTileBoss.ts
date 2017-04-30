a4301baea3a4a52c3045065e2cd600fc23568e43
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-04-29 23:09:07 -0500

    bossork

---
+++ src/core/map/tiles/EventTileBoss.ts
@@ -0,0 +1,24 @@
+import EventTile from '../EventTile';
+import ItemId from '../../item/ItemId';
+
+export function EventTileForagable(title:string,itemId:ItemId){
+    return new EventTile({
+        onEnter: function(bag){
+            if(bag.runCount == 0){
+                bag.sendPartyMessage(`There's a small patch of ${title} shrubs.`);
+            }
+        },
+        onInteract: function(bag){
+            if(bag.runCount == 0){
+                bag.sendPartyMessage(`${bag.player.title} found ${title}!`);
+
+                bag.game.grantPlayerItem(bag.player.uid,itemId,1);
+
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    });
+}
