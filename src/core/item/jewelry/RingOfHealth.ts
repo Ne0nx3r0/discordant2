57da37e3a0a037ba83706eeda8745520577caba9
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-05-14 21:17:17 -0500

    health update

--- src/core/item/jewelry/RingOfHealth.ts
+++ src/core/item/jewelry/RingOfHealth.ts
@@ -6,7 +6,7 @@ import { ICreatureStatSet } from '../../creature/Creature';
 export const RingOfHealth = new ItemEquippable({
     id: ItemId.RingOfHealth,
     title: 'Ring of Health',
-    description: `(+1 Vitality) Wealthy elites will pay anything to prolong their lives as they come to an end, and so it's no surprise that in the wake of the birth of wishes a variety of jewelry was comissioned to accomplish this task.\n\nThis particular example is quite inferior and was likely discarded or sold to a buyer ignorant of its poor quality.`,
+    description: `(+1 Vitality) Wealthy elites will pay anything to prolong their lives as they come to an end. So it was no surprise after the birth of wishes that a variety of jewelry was comissioned to improve the health of the wearer.\n\nThis particular example is quite inferior and was likely discarded or sold to a buyer ignorant of its poor quality.`,
     goldValue: 100,
     slotType:'ring',
     onAddBonuses:function(stats:ICreatureStatSet){
