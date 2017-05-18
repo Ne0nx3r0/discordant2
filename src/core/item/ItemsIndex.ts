2d2c990a08e2cf20443367a3921b5703a1d817a8
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-05-17 23:38:07 -0500

    test

--- src/core/item/ItemsIndex.ts
+++ src/core/item/ItemsIndex.ts
@@ -25,3 +25,4 @@ export { RingOfAgility } from './jewelry/RingOfAgility';
 export { RingOfStrength } from './jewelry/RingOfStrength';
 export { RingOfHealth } from './jewelry/RingOfHealth';
 export { WornLeatherHelmet } from './clothing/WornLeatherHelmet';
+export { TabletOfFire } from './clothing/TabletOfFire';
