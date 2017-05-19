bf80b292d907b81193ed1c1c405c4e233414d766
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-05-18 21:18:14 -0500

    better message for ring of fortune

--- src/core/item/jewelry/RingOfFortune.ts
+++ src/core/item/jewelry/RingOfFortune.ts
@@ -6,7 +6,7 @@ import { ICreatureStatSet } from '../../creature/Creature';
 export const RingOfFortune = new ItemEquippable({
     id: ItemId.RingOfFortune,
     title: 'Ring of Fortune',
-    description: `(+Magic Find) In search of a way to wish for more wishes, Southern philosophers instead discovered a technique which imbues a thing with the very essence of fortune, granting the wearer a sixth sense about where to look for treasures.`,
+    description: `(+10 Magic Find) In search of a way to wish for more wishes, Southern philosophers instead discovered a technique which imbues a thing with the very essence of fortune, granting the wearer a sixth sense about where to look for treasures.`,
     goldValue: 100,
     slotType:'ring',
     onAddBonuses:function(stats:ICreatureStatSet){
