7b64ba239dc39a1232bd6d576fa5f982167b3906
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-05-23 21:40:04 -0500

    tunic drops

--- assets/maps/WesternGate2/WesternGate2Events.ts
+++ assets/maps/WesternGate2/WesternGate2Events.ts
@@ -17,6 +17,7 @@ lootGenerator.addLootItem('common',ItemId.WoodShield,0.3);
 lootGenerator.addLootItem('common',ItemId.HandAxe,0.1);
 lootGenerator.addLootItem('common',ItemId.WornLeathers,0.3);
 lootGenerator.addLootItem('common',ItemId.WornLeatherHelmet,0.3);
+lootGenerator.addLootItem('common',ItemId.ClothTunic,0.3);
 lootGenerator.addLootItem('common',ItemId.Vial,1);
 lootGenerator.addLootItem('common',ItemId.RedForestMapPiece,0.3);
 
