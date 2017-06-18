1e8a27edf2c4878f6c9b99fb703a3becb8a22d07
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-06-18 12:11:12 -0500

    sorting items

--- src/core/item/ItemsIndex.ts
+++ src/core/item/ItemsIndex.ts
@@ -1,26 +1,19 @@
-import { WornLeatherHelmet } from './clothing/WornLeatherHelmet';
-import { AmuletOfHealth } from './jewelry/AmuletOfHealth';
-export { BareHands } from './weapons/BareHands';
-export { HandAxe } from './weapons/HandAxe';
-export { WoodShield } from './weapons/WoodShield';
-export { HuntingSword } from './weapons/HuntingSword';
+//Armor
 export { WornLeathers } from './clothing/WornLeathers';
-export { TabletOfHealing } from './weapons/TabletOfHealing';
-export { TabletOfPoison } from './weapons/TabletOfPoison';
-export { SonicLongsword } from './weapons/SonicLongsword';
-export { Dagger } from './weapons/Dagger';
-export { StoneAxe } from './weapons/StoneAxe';
-export { StoneDagger } from './weapons/StoneDagger';
-export { Vial } from './misc/Vial';
-export { ScarabToken } from './misc/ScarabToken';
-export { Sage } from './misc/Sage';
-export { Yerba } from './misc/Yerba';
-export { Agave } from './misc/Agave';
-export { Bane } from './misc/Bane';
-export { Acai } from './misc/Acai';
-export { Fox } from './misc/Fox';
-export { AlphaKey } from './misc/AlphaKey';
-export { Tent } from './misc/Tent';
+export { WornLeatherHelmet } from './clothing/WornLeatherHelmet';
+
+export { FireAntHelmet } from './clothing/FireAntHelmet';
+export { FireAntArmor } from './clothing/FireAntArmor';
+
+export { ClothHood } from './clothing/ClothHood';
+export { ClothTunic } from './clothing/ClothTunic';
+
+//Shields
+export { WoodShield } from './weapons/WoodShield';
+export { FireAntShield } from './weapons/FireAntShield';
+
+//Jewelry
+import { AmuletOfHealth } from './jewelry/AmuletOfHealth';
 export { RingOfFortune } from './jewelry/RingOfFortune';
 export { RingOfAgility } from './jewelry/RingOfAgility';
 export { RingOfStrength } from './jewelry/RingOfStrength';
@@ -32,25 +25,52 @@ export { AmuletOfStrength } from './jewelry/AmuletOfStrength';
 export { AmuletOfLuck } from './jewelry/AmuletOfLuck';
 export { AmuletOfSpirit } from './jewelry/AmuletOfSpirit';
 export { AmuletOfHealth } from './jewelry/AmuletOfHealth';
-export { WornLeatherHelmet } from './clothing/WornLeatherHelmet';
-export { TabletOfFire } from './weapons/TabletOfFire';
-export { MapAfterRedForestPiece } from './maps/MapAfterRedForestPiece';
-export { RedForestMapPiece } from './maps/RedForestMapPiece';
-export { RedForestMap } from './maps/RedForestMap';
-export { FireAntCarapace } from './misc/FireAntCarapace';
+export { RedEyeRing } from './jewelry/RedEyeRing';
+
+//Consumables
+export { Vial } from './misc/Vial';
+export { Sage } from './misc/Sage';
+export { Yerba } from './misc/Yerba';
+export { Agave } from './misc/Agave';
+export { Bane } from './misc/Bane';
+export { Acai } from './misc/Acai';
+export { Fox } from './misc/Fox';
+export { Tent } from './misc/Tent';
 export { BirthdayCake } from './misc/BirthdayCake';
 export { Revive } from './misc/Revive';
-export { RedEyeRing } from './jewelry/RedEyeRing';
-export { ClothTunic } from './clothing/ClothTunic';
+
+//Maps
+export { MapAfterRedForestPiece } from './maps/MapAfterRedForestPiece';
 export { MapAfterRedForest } from './maps/MapAfterRedForest';
-export { FireAntArmor } from './clothing/FireAntArmor';
-export { FireAntHelmet } from './clothing/FireAntHelmet';
-export { FireAntShield } from './weapons/FireAntShield';
-export { ClothHood } from './clothing/ClothHood';
+
+export { RedForestMapPiece } from './maps/RedForestMapPiece';
+export { RedForestMap } from './maps/RedForestMap';
+
+export { RedForestCastleMapPiece } from './maps/RedForestCastleMapPiece';
+export { RedForestCastleMap } from './maps/RedForestCastleMap';
+
+//Tablets
+export { TabletOfHealing } from './weapons/TabletOfHealing';
+export { TabletOfPoison } from './weapons/TabletOfPoison';
 export { TabletOfFaith } from './weapons/TabletOfFaith';
+export { TabletOfFire } from './weapons/TabletOfFire';
+
+//Collectables
+export { ScarabToken } from './misc/ScarabToken';
+export { AlphaKey } from './misc/AlphaKey';
+
+//Weapons
+export { BareHands } from './weapons/BareHands';
+export { HandAxe } from './weapons/HandAxe';
+export { HuntingSword } from './weapons/HuntingSword';
+export { SonicLongsword } from './weapons/SonicLongsword';
+export { Dagger } from './weapons/Dagger';
+export { StoneAxe } from './weapons/StoneAxe';
+export { StoneDagger } from './weapons/StoneDagger';
+export { FireAntCarapace } from './misc/FireAntCarapace';
 export { FireDagger } from './weapons/FireDagger';
 export { FireAxe } from './weapons/FireAxe';
 export { FireMace } from './weapons/FireMace';
-export { RedForestCastleMapPiece } from './maps/RedForestCastleMapPiece';
-export { RedForestCastleMap } from './maps/RedForestCastleMap';
+
+//For Testing
 export { PrismBlade } from './weapons/PrismBlade';
