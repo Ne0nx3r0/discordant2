acccfd878793914c8c43e83835f8bca60ee74728
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-06-19 22:09:47 -0500

    show wish catcher in items

--- src/core/item/misc/WishCatcher.ts
+++ src/core/item/misc/WishCatcher.ts
@@ -8,6 +8,7 @@ export const WishCatcher = new ItemEquippable({
     title: 'Wish Catcher',
     description: `(2% bonus to wishes earned) As wishes replaced gold as the de facto currency of the land, greedy collectors comissioned these strange white orbs which aided them in the collection of wild wishes.`,
     goldValue: 250,
+    showInItems: true,
     slotType:'pouch',
     onAddBonuses:function(stats:ICreatureStatSet){
         stats.wishBonus += 0.02;
