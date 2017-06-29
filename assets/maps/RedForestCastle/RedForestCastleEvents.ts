9dd8d8d3fd28606879336d2286fef8235632e6e7
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-06-28 21:34:23 -0500

    thingy

--- assets/maps/RedForestCastle/RedForestCastleEvents.ts
+++ assets/maps/RedForestCastle/RedForestCastleEvents.ts
@@ -57,14 +57,12 @@ lootGenerator.addLootItem('uncommon',ItemId.Tent,1);
 lootGenerator.addLootItem('uncommon',ItemId.RedForestCastleMapPiece,1);
 lootGenerator.addLootItem('uncommon',ItemId.MageRobe,0.25);
 lootGenerator.addLootItem('uncommon',ItemId.SonicLongsword,0.1);
-lootGenerator.addLootItem('uncommon',ItemId.StoneAxe,0.1);
+lootGenerator.addLootItem('uncommon',ItemId.StoneDagger,0.1);
 lootGenerator.addLootItem('uncommon',ItemId.RedEyeRing,0.1);
 lootGenerator.addLootItem('uncommon',ItemId.WishCatcher,0.1);
 
 lootGenerator.addLootItem('rare',ItemId.TabletOfFaith,0.1);
-lootGenerator.addLootItem('rare',ItemId.SonicLongsword,0.1);
 lootGenerator.addLootItem('rare',ItemId.StoneAxe,0.1);
-lootGenerator.addLootItem('rare',ItemId.StoneDagger,0.1);
 lootGenerator.addLootItem('rare',ItemId.FireSpear,0.1);
 lootGenerator.addLootItem('rare',ItemId.WishCatcher,0.1);
 lootGenerator.addLootItem('rare',ItemId.FireDagger,0.1);
