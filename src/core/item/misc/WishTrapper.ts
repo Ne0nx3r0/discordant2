49648fc8d05e937220771de5723d7ee47aadcc51
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-07-01 03:01:55 -0500

    note item breaks on death

--- src/core/item/misc/WishTrapper.ts
+++ src/core/item/misc/WishTrapper.ts
@@ -6,7 +6,7 @@ import { ICreatureStatSet } from '../../creature/Creature';
 export const WishTrapper = new ItemEquippable({
     id: ItemId.WishTrapper,
     title: 'Wish Trapper',
-    description: `(Wishes retained on death) A curious device intended to retain the wishes of its carrier in situations where they would normally escape and become wild, ingrained in the surrounding flora and fauna.`,
+    description: `(Wishes retained on death, but the item breaks) A curious device intended to retain the wishes of its carrier in situations where they would normally escape and become wild, ingrained in the surrounding flora and fauna.`,
     goldValue: 500,
     showInItems: true,
     slotType: 'pouch',
