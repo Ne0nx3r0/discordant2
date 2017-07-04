b850cbe92d4b67dddb17bb5f6c7b2720e2b81546
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-07-03 21:52:00 -0500

    ping fix

--- src/bot/commands/Ping.ts
+++ src/bot/commands/Ping.ts
@@ -16,6 +16,12 @@ export default class Pong extends Command{
     async run(bag:CommandRunBag){
         const timeMS = Date.now() - bag.message.createdAt.getTime();
 
+        if(bag.params[0] == 'localhost' || bag.params[0] == '127.0.0.1' || bag.params[0] == '::1'){
+            bag.message.channel.send(`There's no place like home...`);
+
+            return;
+        }
+
         const time = Math.round(timeMS/10)/100;
 
         bag.message.channel.send(`Pong! (${time}s)`);
