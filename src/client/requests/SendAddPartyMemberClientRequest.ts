dff1987192b28c9d5f5d7cfcf5b22ceafbe008dc
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-07-03 21:39:07 -0500

    cleaning up ids

--- src/client/requests/SendAddPartyMemberClientRequest.ts
+++ src/client/requests/SendAddPartyMemberClientRequest.ts
@@ -13,8 +13,6 @@ export default class SendAddPartyMemberClientRequest extends ClientRequest{
     
     async receive(bag:ClientRequestReceiveBag,data:ClientRequestSendAddPartyMemberData):Promise<void>{
         bag.bot.grantPlayerWriteAccessToChannel(bag.channel,data.playerUid);
-
-        bag.bot.addChatRole(data.playerUid,'331616887958142981');
         
         bag.channel.send(`<@${data.playerUid}> joined the party!`);
     }
