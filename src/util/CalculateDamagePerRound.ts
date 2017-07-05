aab36c62ef0451167a994ba0a985717eb7350a5e
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-07-04 23:01:04 -0500

    maths fix

--- src/util/CalculateDamagePerRound.ts
+++ src/util/CalculateDamagePerRound.ts
@@ -1,16 +1,17 @@
 import WeaponAttack from '../core/item/WeaponAttack';
 
-export function CalculateDamagePerRoundDirectly(minDamage:number,maxDamage:number,charges:number,criticalMultiplier:number,criticalChance:number):number{
-    return (minDamage+maxDamage) / 2 * (1 + criticalMultiplier * criticalChance )
-        / 
-        charges;
+export function CalculateDamagePerRoundDirectly(minDamage:number,maxDamage:number,chargesRequired:number,criticalMultiplier:number,criticalChance:number):number{
+    const exactDPR = ((minDamage+maxDamage)/2)/(chargesRequired+1)*((1-criticalChance)+criticalMultiplier*criticalChance);
+
+    //Round to two decimal points
+    return Math.round(exactDPR * 100) / 100;
 }
 
 export default function CalculateDamagePerRound(attack:WeaponAttack){
     return CalculateDamagePerRoundDirectly(
         attack.minBaseDamage,
         attack.maxBaseDamage,
-        attack.chargesRequired+1,
+        attack.chargesRequired,
         attack.weapon.criticalMultiplier,
         attack.weapon.chanceToCritical
     );    
