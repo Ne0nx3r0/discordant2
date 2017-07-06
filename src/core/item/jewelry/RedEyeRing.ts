879ed7725ad293dc960608b9e6660b91ea84ff4b
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-07-05 21:13:34 -0500

    red eye ring description

--- src/core/item/jewelry/RedEyeRing.ts
+++ src/core/item/jewelry/RedEyeRing.ts
@@ -6,7 +6,7 @@ import { ICreatureStatSet } from '../../creature/Creature';
 export const RedEyeRing = new ItemEquippable({
     id: ItemId.RedEyeRing,
     title: 'Red Eye Ring',
-    description: `(3x chance of getting attacked while in a battle) A remarkable gem, the red-eyed sapphire is a blue crystal containing a glowing red center. The beasts and demons that took to wandering the plains after the great collapse are oddly attracted to them, as though the gems contain some form of salvation.`,
+    description: `(increase chance of getting attacked while in a battle) A remarkable gem, the red-eyed sapphire is a blue crystal containing a glowing red center. The beasts and demons that took to wandering the plains after the great collapse are oddly attracted to them, as though the gems contain some form of salvation.`,
     goldValue: 250,
     slotType:'ring',
     onAddBonuses:function(stats:ICreatureStatSet){
--- src/core/permissions/PermissionRoles.Development.ts
+++ src/core/permissions/PermissionRoles.Development.ts
@@ -10,64 +10,15 @@ export const AnonymousPermissions = [
     PermissionId.Classes,
 ];
 
-export const PlayerPermissions = [
-    PermissionId.Stats,
-    PermissionId.WishCalc,
-    PermissionId.Party,
-    PermissionId.Items,
-    PermissionId.PartyNew,
-    PermissionId.PartyInvite,
-    PermissionId.PartyJoin,
-    PermissionId.PartyExplore,
-    PermissionId.PartyMove,
-    PermissionId.Battle,
-    PermissionId.BattleAttack,
-    PermissionId.BattleOffhand,
-    PermissionId.BattleCharge,
-    PermissionId.BattleBlock,
-    PermissionId.Inventory,
-    PermissionId.Help,
-    PermissionId.Give,
-    PermissionId.Equip,
-    PermissionId.Unequip,
-    PermissionId.Use,
-    PermissionId.Challenge,
-    PermissionId.Item,
-    PermissionId.Echo,
-    PermissionId.Embed,
-    PermissionId.ChannelId,
-    PermissionId.Reset,
-    PermissionId.Shutdown,
-    PermissionId.Grant,
-    PermissionId.SetRole,
-    PermissionId.SetPlayingGame,
-    PermissionId.Lockdown,
-    PermissionId.Buy,
-    PermissionId.MarketSell,
-    PermissionId.MarketStop,
-    PermissionId.MarketSearch,
-    PermissionId.MarketNew,
-    PermissionId.MarketBuy,
-    PermissionId.Shop,
-    PermissionId.MarketList,
-    PermissionId.Sell,
-    PermissionId.Lead,
-    PermissionId.Pass,
-    PermissionId.PartyLeave,
-    PermissionId.DPR,
-    PermissionId.Ping,
-    PermissionId.BattleRun,
-    PermissionId.Refresh,
-    PermissionId.Craft,
-    PermissionId.Pay,
-    PermissionId.DodgeCalc,
-    PermissionId.Daily,
-    PermissionId.Heal,
-    PermissionId.SCalc,
-    PermissionId.PartyKick,
-    PermissionId.PartyTransfer,
-    PermissionId.Tester,
-].concat(AnonymousPermissions).sort();
+//indev players get all permissions
+export const PlayerPermissions = Object
+.keys(PermissionId)
+.filter(function(key){
+    return typeof PermissionId[key] === 'number';
+})
+.map(function(key){
+    return PermissionId[key];
+}).concat(AnonymousPermissions).sort();
 
 export const TesterPermissions = [
 ].concat(PlayerPermissions).sort();
