a2e7acec8de0f481e699ac9d7a5310e5da42a981
Ne0nx3r0 <ne0nx3r0@gmail.com>
2018-04-26 23:12:42 -0500

    Add dagger as western gate drop

--- assets/maps/WesternGate2/WesternGate2Events.ts
+++ assets/maps/WesternGate2/WesternGate2Events.ts
@@ -21,6 +21,7 @@ lootGenerator.addLootItem('common',ItemId.WornLeatherHelmet,0.3);
 lootGenerator.addLootItem('common',ItemId.ClothTunic,0.3);
 lootGenerator.addLootItem('common',ItemId.ClothHood,0.3);
 lootGenerator.addLootItem('common',ItemId.Vial,1);
+lootGenerator.addLootItem('common',ItemId.Dagger,0.6);
 lootGenerator.addLootItem('common',ItemId.RedForestMapPiece,0.6);
 
 lootGenerator.addLootItem('rare',ItemId.RingOfAgility,0.1);
