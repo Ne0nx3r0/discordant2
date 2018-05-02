f35f5e7d1cc3a785a165eb720dad00d8287bb495
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-01 22:11:52 -0500

    +50HP at water

--- src/core/map/tiles/EventTileDrinkableWater.ts
+++ src/core/map/tiles/EventTileDrinkableWater.ts
@@ -17,11 +17,11 @@ export class EventTileDrinkableWater extends EventTile{
     onInteract(bag:EventTileHandlerBag):boolean {
         bag.party.members.forEach(function(member){
             if(member.hpCurrent < member.stats.hpTotal){
-                member.hpCurrent = Math.min(member.stats.hpTotal,member.hpCurrent+20);
+                member.hpCurrent = Math.min(member.stats.hpTotal,member.hpCurrent+50);
             }
         });
 
-        bag.party.sendChannelMessage(`The party rests and has a refreshing drink of water! (+20hp to all party members)`);
+        bag.party.sendChannelMessage(`The party rests and has a refreshing drink of water! (+50hp to all party members)`);
 
         if(0.25 > Math.random()){
             bag.party.randomMonsterEncounter();
