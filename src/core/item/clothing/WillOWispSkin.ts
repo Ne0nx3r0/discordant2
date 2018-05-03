e879c22656b3a58ac57ed75ee6a3fcc4db4e2222
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-02 20:48:18 -0500

    will o wisp special

--- src/core/item/clothing/WillOWispSkin.ts
+++ src/core/item/clothing/WillOWispSkin.ts
@@ -1,4 +1,4 @@
-import ItemEquippable from '../ItemEquippable';
+import ItemEquippable, { OnDefendHandlerBag } from '../ItemEquippable';
 import ItemId from '../ItemId';
 import { EquipmentSlot } from '../CreatureEquipment';
 import { ICreatureStatSet } from '../../creature/Creature';
@@ -12,6 +12,14 @@ export const WillOWispSkin = new ItemEquippable({
     onAddBonuses: function(stats:ICreatureStatSet){
         stats.resistances.dark -= 20;
         stats.resistances.thunder += 20;
-        stats.dodge += 1000;
+    },
+    onDefend: function(bag: OnDefendHandlerBag){
+        if(Math.random() <= 0.3){
+            bag.battle.queueBattleMessage([
+                `${bag.defender.creature.title} blinked out of existence and the attack MISSED!`
+            ]);
+            return false;
+        }
+        return true;
     },
 });
