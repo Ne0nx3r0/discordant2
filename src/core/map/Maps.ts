227187a0244edb706416a561734999eab69aae7f
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-05 00:40:41 -0500

    disable living woods for now

--- src/core/map/Maps.ts
+++ src/core/map/Maps.ts
@@ -10,6 +10,8 @@ import { LivingWoodsMapPiece } from '../item/maps/LivingWoodsMapPiece';
 import { LivingWoodsMap } from '../item/maps/LivingWoodsMap';
 import { RedForestCastleMapPiece } from '../item/maps/RedForestCastleMapPiece';
 import { RedForestCastleMap } from '../item/maps/RedForestCastleMap';
+import { GoblinSneak } from '../creature/CreaturesIndex';
+import GoblinSneakWeapon from '../item/weapons/GoblinSneakWeapon';
 
 export const MapWesternGate = new ExplorableMap({
     fileName: 'WesternGate2',
@@ -42,7 +44,7 @@ export const MapLivingWoods = new ExplorableMap({
     mapJson: require('../../../assets/maps/LivingWoods/LivingWoodsLayout.json'),
     mapData: LivingWoodsEvents,
     pieceItem: LivingWoodsMapPiece,
-    mapItem: LivingWoodsMap,
+    mapItem: GoblinSneakWeapon,
 });
 
 export const WorldMaps = {
