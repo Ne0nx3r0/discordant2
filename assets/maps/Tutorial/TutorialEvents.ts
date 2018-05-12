6de587224d5fd71eb33f8c5a1838bb866bdd1cd2
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-11 20:57:05 -0500

    tweaking tutorials

--- assets/maps/Tutorial/TutorialEvents.ts
+++ assets/maps/Tutorial/TutorialEvents.ts
@@ -1,4 +1,5 @@
 import { IMapData } from "../../../src/core/map/IMapData";
+import EventTileEnterMessage from "../../../src/core/map/tiles/EventTileEnterMessage";
 
 export const TutorialEvents:IMapData = {
     startX: 6,
@@ -8,6 +9,17 @@ export const TutorialEvents:IMapData = {
         
     ],
     eventTiles: [
-
+        {
+            coords: [
+                {
+                    x: 6,
+                    y: 17,
+                },
+            ],
+            event: new EventTileEnterMessage({
+                stopsPlayer: true,
+                message: 'Here\'s where you are right now!\n\nTry typing `dpm left`',
+            }),
+        }
     ],
 };
