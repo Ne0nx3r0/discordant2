e40ed6151ba2cc540249a3be2339139f5f31202a
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-11 20:44:16 -0500

    tutorial assets test

---
+++ assets/maps/Tutorial/TutorialEvents.ts
@@ -0,0 +1,13 @@
+import { IMapData } from "../../../src/core/map/IMapData";
+
+export const TutorialEvents:IMapData = {
+    startX: 6,
+    startY: 17,
+    encounterChance: 0,
+    encounters:[
+        
+    ],
+    eventTiles: [
+
+    ],
+};
