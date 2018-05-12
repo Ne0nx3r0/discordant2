8b123a3dae368ba860562d1f9b4b5a5e69269f63
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-12 15:01:28 -0500

    tweak cloth items

--- src/core/item/clothing/ClothTunic.ts
+++ src/core/item/clothing/ClothTunic.ts
@@ -4,10 +4,13 @@ import ItemId from '../ItemId';
 export const ClothTunic = new ItemEquippable({
     id: ItemId.ClothTunic,
     title: 'Cloth Tunic',
-    description: `(+1 Physical Resistance) A very basic robe worn by commoners and adventurers alike.`,
+    description: `(+1 All resistances) A very basic robe worn by commoners and adventurers alike.`,
     goldValue: 1,
     slotType:'armor',
     onAddBonuses:(e)=>{
         e.target.stats.resistances.physical += 1;
+        e.target.stats.resistances.fire += 1;
+        e.target.stats.resistances.thunder += 1;
+        e.target.stats.resistances.dark += 1;
     }
 });
