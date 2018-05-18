ef4cc6a8f514c25fe225048d2a8523b52806949b
Ne0nx3r0 Laptop <ne0nx3r0@gmail.com>
2018-05-17 22:09:56 -0500

    Pet stable costs

---
+++ src/core/pets/StableCosts.ts
@@ -0,0 +1,11 @@
+export const STALL_COST_TO_UPGRADE:number[] = [0,/*not used*/ 1000,//1 stall cost to buy 2nd
+    2500,//2 stalls cost to buy a third
+    5000,
+    7500,
+    10000,
+    20000,
+    30000,
+    40000,
+    50000,
+    100000,
+];
